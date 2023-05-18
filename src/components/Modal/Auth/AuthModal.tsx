04f09cab79a06fd263a8337680afe9ba1789ae5c
myxsan <yksl3461@gmail.com>
2023-05-18 18:57:23 +0300

    q

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,38 @@
+import { authModalState } from "@/src/atoms/authModalAtom";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Modal body</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
